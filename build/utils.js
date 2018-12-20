f52094d5a18937eb5b31dd9cce04f11ac0b0c55f
wuziliang <1002034576@qq.com>
2018-12-20 15:21:50 +0800

    添加了按项目构建.

--- build/utils.js
+++ build/utils.js
@@ -112,16 +112,24 @@ var PAGE_PATH = path.resolve(__dirname, '../src/pages')
 // 用于做相应的merge处理
 var merge = require('webpack-merge')
 
+// 拿到命令行里参数，比如执行（npm run build projectA）,这个时候projectName就等于projectA
+// 有了这个变量，就可以根据这个名字来读取projectConfig里面的配置了
+let projectName = process.argv[2] || 'all';
 
 //多入口配置
 // 通过glob模块读取pages文件夹下的所有对应文件夹下的js后缀文件，如果该文件存在
 // 那么就作为入口处理
 exports.entries = function () {
-    var entryFiles = glob.sync(PAGE_PATH + '/*/*.js')
-    var map = {}
+    var map = {};
+    var entryFiles;
+    if (projectName === 'all') {
+      entryFiles = glob.sync(PAGE_PATH + '/*/*.js')
+    } else {
+      entryFiles = glob.sync(PAGE_PATH + '/' + projectName + '/*.js')
+    }
     entryFiles.forEach((filePath) => {
-        var filename = filePath.substring(filePath.lastIndexOf('\/') + 1, filePath.lastIndexOf('.'))
-        map[filename] = filePath
+      var filename = filePath.substring(filePath.lastIndexOf('\/') + 1, filePath.lastIndexOf('.'))
+      map[filename] = filePath
     })
     return map
 }
@@ -129,7 +137,12 @@ exports.entries = function () {
 //多页面输出配置
 // 与上面的多页面入口配置相同，读取pages文件夹下的对应的html后缀文件，然后放入数组中
 exports.htmlPlugin = function () {
-    let entryHtml = glob.sync(PAGE_PATH + '/*/*.html')
+    let entryHtml;
+    if (projectName === 'all') {
+      entryHtml = glob.sync(PAGE_PATH + '/*/*.html');
+    } else {
+      entryHtml = glob.sync(PAGE_PATH + '/' + projectName + '/*.html');
+    }
     let arr = []
     entryHtml.forEach((filePath) => {
         let filename = filePath.substring(filePath.lastIndexOf('\/') + 1, filePath.lastIndexOf('.'))
